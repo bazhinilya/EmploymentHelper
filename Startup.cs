ca4f73eeddbe1a0b534bd7688b8944d1526885b7
bazhinilya <ilya_bazhin@live.ru>
2022-05-01 22:39:13 +0300

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace EmploymentHelper
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(swg => swg.SwaggerDoc("v1", new OpenApiInfo { Title = "EmploymentHelper", Version = "v1" }));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(swgUi => swgUi.SwaggerEndpoint("/swagger/v1/swagger.json", "EmploymentHelper v1"));
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
